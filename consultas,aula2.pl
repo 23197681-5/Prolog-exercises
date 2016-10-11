126a1044cb7063cf5f71d5cf73dcc6d62999cac5
Willian Lauber <lcwillian_hd@hotmail.com>
2016-10-11 20:13:37 -0300

    Update consultas,aula2.pl

--- consultas,aula2.pl
+++ consultas,aula2.pl
@@ -1,2 +1,6 @@
 actor(Filme, kevin_spacey, _), movie(Filme, Ano).
 movie(Filme, Ano), Ano>1999.
+actress(Filme, Atriz, _); movie(Filme, Ano); Ano>2005.
+actress(Filme, Atriz, Personagem), Atriz = Personagem.
+aggregate_all(count, (actress(Filme, Atriz, Personagem), movie(Filme, Ano), Ano>2005), Count).
+(actress(_, Atriz, Personagem), Atriz = Personagem) ;(actor(_, Ator, Personagem), Ator = Personagem).
