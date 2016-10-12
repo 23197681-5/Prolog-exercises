fcef487d5bf2049f77b8957479d4cd25c37f74d1
Willian Lauber <lcwillian_hd@hotmail.com>
2016-10-11 21:12:48 -0300

    Update testehainstem.pl

--- testehainstem.pl
+++ testehainstem.pl
@@ -1,6 +1,6 @@
-casas(L):-
-    length(L, 3), %tamanho
-    nextto([vermelha, _], [_, espanhol],L), %dica1
-    member([azul, alemao], L), %dica2
-    L=[_, [_, italiano], _], %dica3
-    member([preta, _], L). %dica4
+casas(Casas):-
+    length(Casas, 3), %tamanho
+    nextto([vermelha, _], [_, espanhol],Casas), %dica1
+    member([azul, alemao], Casas), %dica2
+    Casas=[_, [_, italiano], _], %dica3
+    member([preta, _], Casas). %dica4
